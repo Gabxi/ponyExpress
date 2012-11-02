8f8e66e07e968a174167f8811f5621e594d35a45
Gabriel Kho <gabriel.d.kho@gmail.com>
2012-11-02 09:51:46 -0700

    Updated

--- scrubber.py
+++ scrubber.py
@@ -1,30 +1,14 @@
-f = open('postOfficeConvert.csv', 'r')
-x = open('postOffice.json', 'w')
+f = open('final.csv', 'r')
+x = open('postOfficeFinal.json', 'w')
 for line in f:
 	str = ""
 	arr = line.split(",")
-	str += arr[0] + ":["
+	str += arr[0][:-1] +  arr[3][1:-1] + arr[4][1:]+ ":["
 	str += arr[1] + ","
-	str += arr[2] + ","
 	str += arr[3] + ","
 	str += arr[4] + ","
-	str += arr[5] + ","
+	str += arr[5][1:-1] + ","
 	str += arr[6][1:-1] + ","
-	str += arr[7][1:-1] + ","
-	str += arr[8] + ","
-	str += arr[9] + ","
-	str += arr[10] + ","
-	str += arr[11] + ","
-	str += arr[12] + ","
-	str += arr[13] + ","
-	str += arr[14] + ","
-	str += arr[15] + ","
-	str += arr[16] + ","
-	str += arr[17] + ","
-	str += arr[18] + ","
-	str += arr[19] + ","
-	str += arr[20] + ","
-	str += arr[21] + ","
-	str += arr[22] + ","
-	str += arr[23] + "]" + ","
+	str += arr[7] + ","
+	str += arr[8][:-1] + "]" + ","
  	x.write(str)
